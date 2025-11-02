ec702b2bea6adaaa9f8ceb450a97062b1959000d
khazink <khairellkhazin@gmail.com>
2025-11-02 23:52:07 +0800

    int update

--- print_int.c
+++ print_int.c
@@ -15,17 +15,39 @@ int	print_int(t_string *s, int n)
 	long	num;
 	char	sign;
 	int	lens[3];
+	int	count;
 
 	num = (long)n;
 	sign = get_sign(s, num);
 	if (s->has_prec && s->precision == 0 && num == 0)
 	{
-		lens[2] = s->width;
-		while (len[2]--)
+		count = s->width;
+		while (s->width--)
 			write(1, " ", 1);
-		return (s->width);
+		return (count);
 	}
-	len[0] = get_num_len(num < 0 ? -num : num);
-	len[1] = 0;
-	if
+	calculate_lens(s, num, sign, lens)
+	if (s->minus)
+		return (print_with_left_justify(s, num, sign, lens));
+	if (s->zero && !s->has_prec)
+		return (print_with_zero_padding(s, num, sign, lens));
+	return (print_with_space_padding(num, sign, lens);
+}
+
+static void calculate_lens(t_string *s, long num, char sign, int *lens)
+{
+	long	abs_num;
+
+	abs_num = num;
+	if (num < 0)
+		abs_num = -num;
+	lens = get_num_len(abs_num);
+	lens[1] = 0;
+	if (s->has_prec && s->precision > lens[0])
+		lens[1] = s->precision - lens[0];
+	lens[2] = s->width - (lens[0] + lens[1]);
+	if (sign)
+		lens[2] -= 1;
+	if (lens[2] < 0)
+		lens[2] = 0;
 }
